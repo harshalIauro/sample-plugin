4b27a629e4c70c005ad464760293fa46f506eb18
Harshal <harshal@Harshals-MacBook-Pro.local>
2024-04-19 12:11:59 +0530

    Harshal :- Sample UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { app } from '@microsoft/teams-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample-teams-app';
+
+  ngOnInit() {
+    app.initialize()
+    app.getContext().then((context: app.Context) => {
+      console.log('context', context);
+    }).catch((error: any) => {
+      console.error('Error', error);
+    });
+  }
+
 }
